fb59e4343f41912dd339e42179d2abbe9bd0acad
Cristian <dragos.manta@yahoo.ca>
2020-11-24 16:03:43 -0500

    Added merging script, re-organized directory structure and initialized utils module to reduce redundant code in scripts.

---
+++ scripts/merge.py
@@ -0,0 +1,44 @@
+import argparse
+import json
+
+
+def read_posts(file):
+    posts = []
+    with open(file, 'r') as f:
+        for line in f:
+            post = json.loads(line)
+            print("string")
+            posts.append(post)
+
+    return posts
+
+
+def write_json(posts, output_file):
+    with open(output_file, 'w') as f:
+        for post in posts:
+            line = json.dumps(post)
+            f.write(line)
+            f.write("\n")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--files', type=str, required=True, nargs='+',
+                        help="a list of file paths whose posts you want to merge in this order")
+    parser.add_argument('-o', '--output', type=str, required=True,
+                        help="path to file where to write the merged posts")
+    args = parser.parse_args()
+
+    files_list = args.files
+    output_file = args.output
+
+    collected_posts = []
+
+    for file in files_list:
+        collected_posts.extend(read_posts(file))
+
+    write_json(collected_posts, output_file)
+
+
+if __name__ == "__main__":
+    main()
